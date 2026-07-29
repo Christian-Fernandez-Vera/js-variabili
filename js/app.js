0fa0a81730a5eff83238a79765045487372ea02e
Christian Fernandez Vera <xtian.fdz@hotmail.com>
2026-07-30 00:16:42 +0200

    feat: initialize project structure with HTML boilerplate and user profile variables in app.js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// MODULO 1 - CLASSE 13: ESERCIZIO JS VARIABILI
+
+// 1. Dichiarazione e inizializzazione delle variabili di profilo
+const nome = "Christian";
+const eta = 32;
+const lavoro = "sviluppatore"
